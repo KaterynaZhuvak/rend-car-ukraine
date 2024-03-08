a3d6deba1dac5b5f079f40e6c73ba872d7b28fa1
KaterynaZhuvak <129985868+KaterynaZhuvak@users.noreply.github.com>
2024-03-08 13:07:32 -0500

    updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { StyledHeader } from "./Styled";
+import logoImg from "../../../public/car.png";
 
 const Header = () => {
   return (
     <StyledHeader>
       <div className="logoContainer">
-        <img className="hederImg" src="../../../public/car.png" alt="logo" />
+        <img className="hederImg" src={logoImg} alt="logo" />
         <h1 className="logoTitle">Car Rental</h1>
       </div>
 
